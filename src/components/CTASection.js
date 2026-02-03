a90c155a247123cee8fe6399f29b74f2baa1fe2e
AdieleSolomon <87618504+AdieleSolomon@users.noreply.github.com>
2026-02-03 16:11:17 +0100

    Initial commit

--- src/components/CTASection.js
+++
@@ -1,9 +0,0 @@
-export default function CTASection() {
-  return (
-    <section className="cta">
-      <h2>Ready to Publish?</h2>
-      <p>Contact us today to start your publishing journey</p>
-      <button>Get Started</button>
-    </section>
-  );
-}
--- src/components/FeaturedBooks.js
+++
@@ -1,8 +0,0 @@
-export default function FeaturedBooks() {
-  return (
-    <section className="featured-books">
-      <h2>Featured Books</h2>
-      <div className="books-grid">{/* Books will be displayed here */}</div>
-    </section>
-  );
-}
